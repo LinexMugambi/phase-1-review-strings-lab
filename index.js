5a79c7797cd3899babdfeefd332876f9ae066c85
git_linexmugambi <linexmugambi254@gmail.com>
2022-11-23 16:08:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser ='Grace Hopper';
+//current user is Grace
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//if the welcome message is printed it will say "Welcome to Flatbook, Grace Hopper!"
+//`` is where I found some trouble but i caught it
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//Uppercase version
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
